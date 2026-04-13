7fc83067ff3aac6856101d8b59daf0cfd9135949
samiracode <samiratavakolli.k@gmaile.com>
2026-04-13 10:13:42 +0200

    print logs into monitor.log

--- monitor.py
+++ monitor.py
@@ -1,11 +1,19 @@
 import psutil
 import time
+from datetime import datetime
 
 while True:
     cpu = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
 
-    print(f"CPU: {cpu}% | Memory: {memory}% | Disk: {disk}%")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    log = f"{timestamp} | CPU: {cpu}% | Memory: {memory}% | Disk: {disk}%"
+
+    print(log)
+
+    with open("monitor.log", "a") as file:
+        file.write(log + "\n")
 
     time.sleep(2)
