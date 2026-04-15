d3d239a6858642f1331e36c17e4720444e665d7a
samiracode <samiratavakolli.k@gmaile.com>
2026-04-15 11:00:52 +0200

    adding memory & disk alert

--- monitor.py
+++ monitor.py
@@ -3,6 +3,18 @@ import time
 from datetime import datetime
 
 CPU_THRESHOLD = 80
+MEMORY_THRESHOLD = 50
+DISK_THRESHOLD = 50
+
+
+def check_alert(value, threshold, name, timestamp):
+    if value > threshold:
+        alert = f"⚠️ HIGH {name} ALERT: {value}%"
+        print(alert)
+
+        with open("alerts.log", "a") as f:
+            f.write(f"{timestamp} | {alert}\n")
+
 
 while True:
     cpu = psutil.cpu_percent(interval=1)
@@ -12,18 +24,18 @@ while True:
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     log = f"{timestamp} | CPU: {cpu}% | Memory: {memory}% | Disk: {disk}%"
-
     print(log)
 
-    # ALERT condition
-    if cpu > CPU_THRESHOLD:
-        alert = f"⚠️ HIGH CPU ALERT: {cpu}%"
-        print(alert)
+    # Run alert checks
+    check_alert(cpu, CPU_THRESHOLD, "CPU", timestamp)
+    check_alert(memory, MEMORY_THRESHOLD, "MEMORY", timestamp)
+    check_alert(disk, DISK_THRESHOLD, "DISK", timestamp)
 
-        with open("alerts.log", "a") as f:
-            f.write(f"{timestamp} | {alert}\n")
-
+    # Save monitoring log
     with open("monitor.log", "a") as file:
         file.write(log + "\n")
 
     time.sleep(2)
+
+
+  
